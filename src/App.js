a0f1093426f494a681b0f758427324440cf299f7
Joungwoo Baik <joungwoo.baik@gmail.com>
2019-11-11 12:20:30 -0800

    Add react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React from "react"
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom"
+
 import Homepage from "./components/Homepage"
 import Navigation from "./components/navigation/Navigation"
+
 import "./App.css"
 
 function App() {
   return (
-    <div className='App'>
-      <Navigation />
-      <Homepage />
-    </div>
+    <Router>
+      <div className='App'>
+        <Navigation />
+        <Switch>
+          <Route exact path='/'>
+            <Homepage />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   )
 }
 
--- src/components/platformCategories/category/Category.jsx
+++ src/components/platformCategories/category/Category.jsx
@@ -2,7 +2,12 @@ import React from "react"
 import classes from "./Category.module.css"
 
 const Category = props => {
-  return <div className={classes.Category}>{props.name}</div>
+  return (
+    <div className={classes.Category}>
+      {props.name}
+      <div>picture</div>
+    </div>
+  )
 }
 
 export default Category
